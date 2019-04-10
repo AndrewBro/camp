bd8d9f79a3fb81df3d275d9abf07ac32980c11d0
Andrey <beliverwww@gmail.com>
2019-04-10 10:48:38 +0300

    local

--- src/components/InitialVideo.vue
+++ src/components/InitialVideo.vue
@@ -1,7 +1,8 @@
 <template>
   <div class='initialVideo' v-show="!hidedVideo">
-    <!--<video autoplay controls id='video'>-->
-    <video autoplay id='video'>
+    <button type="button" class="btn btn-outline-danger" @click="hideVideo">Видел я ваш видосик</button>
+    <video autoplay controls id='video'>
+    <!--<video autoplay id='video'>-->
       <source src='./../assets/media/intro.mp4' type='video/mp4'/>
     </video>
   </div>
@@ -22,22 +23,28 @@
       videoInit() {
         const video = document.getElementById('video');
         localStorage.isPlayed && video.pause();
-        if (!localStorage.isPlayed) {
-          // document.getElementById('video').play();
-          video.play();
-          video.addEventListener('ended', (() => {
-            this.hidedVideo = true;
-          }), false);
-          localStorage.setItem('isPlayed', '1')
-        } else {
+        video.play();
+        video.addEventListener('ended', (() => {
           this.hidedVideo = true;
-        }
+        }), false);
+        // if (!localStorage.isPlayed) {
+        //   video.play();
+        //   video.addEventListener('ended', (() => {
+        //     this.hidedVideo = true;
+        //   }), false);
+        //   localStorage.setItem('isPlayed', '1')
+        // } else {
+        //   this.hidedVideo = true;
+        // }
+      },
+      hideVideo () {
+        this.hidedVideo = true
       }
     }
   }
 </script>
 
-<style lang='scss'>
+<style lang='scss' scoped>
   .initialVideo {
     position: absolute;
     top: 0;
@@ -58,5 +65,13 @@
     height: 100%;
     width: 100%;
     background: black;
+  }
+  button {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    z-index: 123123123;
+    /*width: 50px;*/
+    /*height: 50px;*/
   }
 </style>
