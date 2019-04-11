e44b074e27847eb825b9324d6c06f9365d5094b5
Andrey <beliverwww@gmail.com>
2019-04-11 09:39:21 +0300

    added Cost and fonts

---
+++ src/components/Cost.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='cost'>
+    <h1>Cost</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam culpa debitis dolor ducimus error itaque laborum libero magni nisi nostrum, porro quidem rerum, suscipit ullam vitae. Accusantium animi consequuntur corporis culpa delectus dolor doloribus eligendi illo itaque iure magni minus necessitatibus nemo nisi nostrum officiis omnis optio pariatur praesentium quae quam quibusdam ratione recusandae reiciendis rem rerum similique, sunt tempora unde veritatis. Architecto assumenda at cum expedita iure maxime neque officia qui quod reiciendis! Asperiores cumque delectus doloremque excepturi molestias non repellendus sint? Dignissimos facilis laborum obcaecati voluptas voluptate. Atque ex officia quae sequi unde. Eveniet iste nihil recusandae voluptates!
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Cost'
+  }
+</script>
+
+
+<style lang="scss" scoped>
+  @import "../assets/scss/_main.scss";
+</style>
