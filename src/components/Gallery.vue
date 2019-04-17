20aa93890871467ce189a4b32aed06ad09f6b0f3
Andrey <beliverwww@gmail.com>
2019-04-17 10:13:31 +0300

    pulled from master

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,39 +1,37 @@
 <template>
     <div class="gallery">
         <div class="gallery-wrap">
-            <div data-aos="zoom-in-left" data-aos-easing="linear" data-aos-duration="100">>
+            <div data-aos="zoom-in-left" data-aos-easing="linear" data-aos-duration="100">
                 <div class="gallery-title text-center">
-                    <h3>Галерея</h3>
+                    <h3>Воспоминания</h3>
+                    <div class="leaf"></div>
                 </div>
             </div>
             <div data-aos="zoom-in-right" data-aos-easing="linear" data-aos-duration="200">
                 <div class="gallery-grid">
                     <figure class="gallery__item gallery__item--1">
-                        <img src="../assets/images/rectangle-copy-22.png" class="gallery__img" alt="Image 2">
+                    <img src="../assets/images/gallery1.jpg" class="gallery__img" alt="Image 1">
                     </figure>
                     <figure class="gallery__item gallery__item--2">
-                        <img src="../assets/images/rectangle-copy-17.png" class="gallery__img" alt="Image 2">
+                        <img src="../assets/images/gallery5.jpg" class="gallery__img" alt="Image 2">
                     </figure>
                     <figure class="gallery__item gallery__item--3">
-                        <img src="../assets/images/rectangle-copy-14.png" class="gallery__img" alt="Image 3">
+                        <img src="../assets/images/gallery7.jpg" class="gallery__img" alt="Image 3">
                     </figure>
                     <figure class="gallery__item gallery__item--4">
-                        <img src="../assets/images/rectangle-copy-16.png" class="gallery__img" alt="Image 4">
+                        <img src="../assets/images/gallery6.jpg" class="gallery__img" alt="Image 4">
                     </figure>
                     <figure class="gallery__item gallery__item--5">
-                        <img src="../assets/images/rectangle-copy-18.png" class="gallery__img" alt="Image 5">
+                        <img src="../assets/images/gallery4.jpg" class="gallery__img" alt="Image 5">
                     </figure>
                     <figure class="gallery__item gallery__item--6">
-                        <img src="../assets/images/rectangle-copy-15.png" class="gallery__img" alt="Image 6">
+                        <img src="../assets/images/gallery2.jpg" class="gallery__img" alt="Image 6">
                     </figure>
                     <figure class="gallery__item gallery__item--7">
-                        <img src="../assets/images/rectangle-copy-13.png" class="gallery__img" alt="Image 7">
+                        <img src="../assets/images/gallery8.jpg" class="gallery__img" alt="Image 7">
                     </figure>
                     <figure class="gallery__item gallery__item--8">
-                        <img src="../assets/images/rectangle-copy-27.png" class="gallery__img" alt="Image 8">
-                    </figure>
-                    <figure class="gallery__item gallery__item--9">
-                        <img src="../assets/images/rectangle-copy-29.png" class="gallery__img" alt="Image 9">
+                        <img src="../assets/images/gallery3.jpg" class="gallery__img" alt="Image 8">
                     </figure>
                 </div>
             </div>
@@ -53,7 +51,8 @@
     .gallery {
         overflow: hidden;
         height: auto;
-        padding-bottom: 20%
+        padding-bottom: 10%;
+        padding-top: 25px;
     }
 
     .gallery-wrap {
@@ -77,9 +76,9 @@
             }
 
             &:before {
-                top: 5vh;
+                top: -1vh;
                 right: 0;
-                left: -15vw;
+                left: -20vw;
                 height: 100px;
                 bottom: 0;
                 background-image: url("../../src/assets/images/image.png");
@@ -90,9 +89,9 @@
             }
 
             &:after {
-                top: -5vh;
+                top: -14vh;
                 right: 0;
-                left: 30vw;
+                left: 35vw;
                 height: 150px;
                 bottom: -15vh;
                 background-image: url("../../src/assets/images/group.png");
@@ -116,77 +115,131 @@
                 letter-spacing: 1px;
                 color: #21232a;
                 margin: 0 auto 40px;
-                max-width: 300px;
+                max-width: 500px;
+            }
+            .leaf {
+                position: relative;
+                max-width: 960px;
+                &:after,
+                &:before {
+                    position: absolute;
+                    content: '';
+                    background-repeat: no-repeat;
+                    background-position: center;
+                    width: 100%;
+                }
+                &:before {
+                    top: -11px;
+                    right: 0;
+                    left: 8vw;
+                    height: 99px;
+                    bottom: 0;
+                    background-image: url("../../src/assets/images/flower-copy-10.png");
+                    max-width: 200px;
+                    @media (max-width: 900px) {
+                        bottom: -10vh;
+                    }
+                }
+                &:after {
+                    top: -8vh;
+                    right: 0;
+                    left: 25vw;
+                    height: 143px;
+                    bottom: -15vh;
+                    background-image: url("../../src/assets/images/flower-copy-8.png");
+                    @media (max-width: 900px) {
+                        top: 10vh;
+                        right: 0;
+                        left: 45vw;
+                    }
+                }
             }
         }
 
         .gallery-grid {
-            display: flex;
-            justify-content: space-between;
-            flex-flow: row wrap;
-
-            .gallery__img {
-                object-fit: contain;
-                border-radius: 3px;
-            }
-
+            display: grid;
+            grid-template-columns: repeat(8, 1fr);
+            grid-template-rows: repeat(11, 5vw);
+            grid-gap: 15px;
+            max-width: 1200px;
+            margin: 0 auto;
             .gallery__item {
-                &--1 {
-                    height: 150px;
-                    align-self: center;
-                }
-
-                &--2 {
-                    height: 150px;
-                    align-self: center;
-                }
-
-                &--3 {
-                    height: 150px;
-                    align-self: end;
-                }
-
-                &--5 {
-                    height: 150px;
-                    align-self: start;
-
-                    .gallery__img {
-                        width: 80%;
-                    }
-                }
-
-                &--6 {
-                    height: 110px;
-                    align-self: end;
-                }
-
-                &--7 {
-                    height: 55px;
-                    align-self: end;
-                }
-
-                &--8 {
-                    height: 55px;
-                    align-self: end;
-                }
+                cursor: pointer;
+            }
+            figure {
+                margin-bottom: 0;
+            }
+            .gallery__img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                border-radius: 2px;
+                box-shadow: 0 5px 16px -1px rgba(33, 35, 42, 0.04);
+            }
+            .gallery__item--1 {
+                grid-column-start: 1;
+                grid-column-end: 3;
+                grid-row-start: 1;
+                grid-row-end: 4;
+            }
+            .gallery__item--2 {
+                grid-column-start: 3;
+                grid-column-end: 5;
+                grid-row-start: 1;
+                grid-row-end: 4;
+            }
+            .gallery__item--3 {
+                grid-column-start: 5;
+                grid-column-end: 9;
+                grid-row-start: 1;
+                grid-row-end: 4;
+            }
+            .gallery__item--4 {
+                grid-column-start: 1;
+                grid-column-end: 3;
+                grid-row-start: 4;
+                grid-row-end: 8;
+            }
+            .gallery__item--5 {
+                grid-column-start: 3;
+                grid-column-end: 5;
+                grid-row-start: 4;
+                grid-row-end: 9;
+            }
+            .gallery__item--6 {
+                 grid-column-start: 5;
+                 grid-column-end: 9;
+                 grid-row-start: 4;
+                 grid-row-end: 7;
+            }
+            .gallery__item--7 {
+                grid-column-start: 5;
+                grid-column-end: 9;
+                grid-row-start: 7;
+                grid-row-end: 10;
+            }
+            .gallery__item--8 {
+                grid-column-start: 1;
+                grid-column-end: 3;
+                grid-row-start: 8;
+                grid-row-end: 11;
             }
         }
     }
 
-    @media screen and (max-width: 1024px) {
-        .gallery-wrap {
+    @media screen and (max-width: 961px) {
+        .gallery {
+            .gallery-title {
+                h3 {
+                    padding-top: 20px;
+                    font-size: 2.2em;
+                }
+            }
             .gallery-grid {
-                display: flex;
-                justify-content: center;
-
+                display: block;
+                width: 90%;
                 .gallery__item {
-                    width: 90%;
-                    height: 100%;
-                    align-self: start;
-
-                    .gallery__img {
-                        width: 100%;
-                    }
+                    margin-bottom: 20px;
                 }
             }
         }
